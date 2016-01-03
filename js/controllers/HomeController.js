a17196c343e3cd7ef5d4d6f8243e32b9960adf9d
Hubert Siwkin <hsiwkin@gmail.com>
2016-01-03 23:49:55 +0100

    early version of addSuggestion function

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,6 +1,17 @@
-app.controller('HomeController', ['$scope', 'suggestions', '$routeParams',
-    function($scope, suggestions, $routeParams) {
+app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions) {
 
-        $scope.posts = suggestions.posts;
+    $scope.posts = suggestions.posts;
+
+    $scope.addSuggestion = function() {
+        //TODO
+
+        if ($scope.title) {
+            console.log('title: ' + $scope.title);
+            suggestions.posts.push()
+
+            $scope.title = '';
+        }
+
+    }
 
 }]);
